3eda4ded0508b3ab53512056e2c17311ae1c21df
Daniel-Chukwu <ifeanyidaniel1804@gmail.com>
2025-05-05 11:28:42 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count >= 100 ? (count = 100) : count + 1);
+  const decrement = () => setCount(count <= 0 ? (count = 0) : count - 1);
+  const EdgeCases = () => {
+    if (count === 0 || count === 100) {
+      return <p>You have reached the limit !!!</p>;
+    }
+  };
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={increment}>Increase</button>
+      <button onClick={decrement}>Decrease</button>
+      <EdgeCases />
+    </>
+  );
+};
+
+export default App;
